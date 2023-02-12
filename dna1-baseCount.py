7709af69bbfee9f2aaad2ff2238588ce97af2c82
asif-kamal <asif.kamal1489@gmail.com>
2023-02-12 15:14:10 -0500

    Added if statement for total bases written

--- dna1-baseCount.py
+++ dna1-baseCount.py
@@ -2,11 +2,13 @@ dnaIn = open("dna1.dat")
 contents = dnaIn.read()
 dnaIn.close()
 
-input1 = input(("Which pattern?: "))
+input1 = input(("Which pattern? Type 'Total DNA bases' for all bases: "))
 Count = contents.count(input1)
 
 with open("dna1-baseCount.txt", "a") as f:
-    f.write(input1 + ": " + str(Count)+ "\n")
-    f.close()
-
-    
+    if input1 != "Total DNA bases":
+        f.write(input1 + ": " + str(Count)+ "\n")
+    else: 
+        Count = contents.count("A") + contents.count("C") + contents.count("T") + contents.count("G")
+        f.write(input1 + ": " + str(Count)+ "\n")
+f.close()
